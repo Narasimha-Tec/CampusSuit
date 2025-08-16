1cef825f62b5c26cf1dde2ecf2fbc2e04391c420
Tipl31 <Tipl31@tecdatum.com>
2025-08-16 16:10:01 +0530

    Made some updates

---
+++ src/Institute.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Institute(){
+    return (
+        <>
+        <div className="container ">
+       <input type="text" className="form-control" />
+<div className="header">simha</div>
+        </div>
+        </>
+    )
+}
+export default Institute;
